10ddd62d536e08b47e7e96661da331f4252f61a9
blzzrd <AlexCast001@gmail.com>
2019-11-08 23:39:35 -0800

    Added in UNTESTED code for changing the map location.

--- SpaceCraftMovement.js
+++ SpaceCraftMovement.js
@@ -1,46 +1,58 @@
-function moveSpacecraft(angle, distance) {
+function moveSpacecraft(angle, distance) { 
    // Angles: [0 = EAST, 90 = NORTH, 180 = WEST, 270 = SOUTH]
-   xCurrent = document.getElementsByName("xValue")[0].value
-   yCurrent = document.getElementsByName("yValue")[0].value
+   xCurrent = document.getElementsByName("xValue")[0].value;
+   yCurrent = document.getElementsByName("yValue")[0].value;
+   wormhole = false
 
    if (angle == 0) {
       if (xCurrent + distance > Max) {
-         // return wormhole coords and message
+	wormhole = true
       }
       else {
-         // new x = xCurrent + distance
-         // update xValue to be correct.
+	newX = xCurrent + distance;
+	newY = yCurrent;
       }
    }
    else if (angle == 90) {
       if (yCurrent + distance > Max) {
-         // return wormhole coords and message
+	wormhole = true
       }
       else {
-         // update yValue to be correct.
+	newX = xCurrent;
+	newY = yCurrent + distance;
       }
    }
    else if (angle == 180) {
       if (xCurrent < distance) {
-         // return wormhole coords and message
+	wormhole = true
       }
       else {
-         // new x = xCurrent - distance
-         // update xValue to be correct.
+	newX = xCurrent - distance;
+	newY = yCurrent;
       }
    }
    else if (angle == 270) {
       if (yCurrent < distance) {
-         // return wormhole coords
+	wormhole = true
       }
       else {
-         // update yValue to be correct.
+	newX = xCurrent;
+	newY = yCurrent - distance;
       }
    }
-
-
-
+   if(wormhole == true){
+	newX = randCord(Max)
+	newY = randCord(Max)
+	alert("Spaceship traveled out of the galaxy. Warping in a wormhole.")
+   }
+   document.forms[0].xValue = newX
+   document.forms[0].yValue = newY
+   
    alert("Move Space Craft");
    checkEnergy();
    checkSupplies();
+}
+
+function randCord(max) { 
+	return Math.round(Math.random() * max);
 }
