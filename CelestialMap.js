307a59c3b4abac8baa72a5d2f82290465caf5f9b
blzzrd <AlexCast001@gmail.com>
2019-11-13 10:57:16 -0800

    Added changes to the display function [untested]

--- CelestialMap.js
+++ CelestialMap.js
@@ -1,7 +1,8 @@
 // C O N S T A N T S
-var UNK = "-"
-var EMPTY = "+"
-var CELESTIAL_OBJECT = "C"
+var UNK = '-'
+var EMPTY = '+'
+var CELESTIAL_OBJECT = 'C'
+// Expand celestial object to planet, spacestation, asteroid, etc.
 
 class Celestial_Point {
     // I at least think that this is what it's called?
@@ -9,8 +10,7 @@ class Celestial_Point {
         this.type = EMPTY;
         this.visited = false;
         this.visible = false;
-    }
-    
+    } 
     constructor(type, visited, visible) {
         this.type = type;
         this.visited = visited;
@@ -32,12 +32,25 @@ class Celestial_Point {
     hide() {
         this.visible = false;
     }
+
+    display() {	
+	// is a pseudo-display for the celestial map function.
+	if (this.visible == true) {
+	    return this.type;
+	}
+	else {
+	    return UNK;
+	}
+    }
 }
 
 class Celestial_Map {
-    build_map() {
-        size = eval(document.getElementById('mapSize').value);
+    constructor() { 
+	this.size=eval(document.getElementById('mapSize'.value));
+	this.map = build_map(this.size);	
+    }
 
+    build_map(size) {
         // Blank Map Generation 
         var map = [];
         for (var i = 0; i < size; i++) {
@@ -46,15 +59,26 @@ class Celestial_Map {
                 map[i][j] = new Celestial_Point();
             }
         }
+	return map;
     }
 
     display_map() {
+	// displaying it as a string.. 
+	// we have to iterate n^2 times EACH TIME the player 
+	// makes a decision, so we definitely need to revise this.
+	alert(this.map)
+	print_string = '';
+	for (var i = 0; i < this.size; i++) {
+	    print_string += '\n';
+	    for(var j = 0; j < this.size; j++) {
+		print_string += this.map[i][j].display() + ' ';
+	    }
+	}
+
+	return print_string;
         // Check if objects are visible OR visited?
-
         // If objects are visible - display that in map
-
         // If objects are visited - keep that in map
-
         // If objects are not visited and not visible
             // Print UNK in place.
     }
@@ -65,6 +89,8 @@ class Celestial_Map {
         // Do we want to do if type == planet / space station
         // it's visible / invisible? we can arrange that here.
         // That way we don't bloat the functions above? 
+
+	// Otherwise we can add it as another parameter, default FALSE.
     }
 }
 
