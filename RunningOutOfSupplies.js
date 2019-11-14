a10d0f2611434f52b7ef647597d4d318b143f63a
SamYoungs <56419413+SamYoungs@users.noreply.github.com>
2019-11-14 11:19:16 -0800

    Update RunningOutOfSupplies.js

--- RunningOutOfSupplies.js
+++ RunningOutOfSupplies.js
@@ -1,6 +1,11 @@
 function checkSupplies() {
    alert("Checking Supplies");
-   if(eval(UI.supplies.value) == 0) {
-   	alert ("Game Over\nYou ran out of supplies");
-}
+   if(eval(UI.suppliesVal.value) == 0) {
+		if (customSettings.playerDies.value == "true") {
+   			alert ("Game Over\nYou ran out of energy");
+		}
+		else {
+			alert ("Player death is disabled");
+		}
+	}
 }
