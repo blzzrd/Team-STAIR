cb5bc86f35a926e82ccbdcd8f8397880aac8ea02
SamYoungs <56419413+SamYoungs@users.noreply.github.com>
2019-11-14 16:44:46 -0800

    Update RunningOutOfSupplies.js
    refreshing page on game over

--- RunningOutOfSupplies.js
+++ RunningOutOfSupplies.js
@@ -3,6 +3,7 @@ function checkSupplies() {
    if(eval(UI.suppliesVal.value) >= 0) {
 		if (customSettings.playerDies.value == "true") {
    			alert ("Game Over\nYou ran out of supplies");
+			location.reload(false);
 		}
 		else {
 			alert ("Player death is disabled");
