b7c25054bf21bb2618ff5dd2e89f2f1ead9b3906
SamYoungs <56419413+SamYoungs@users.noreply.github.com>
2019-11-14 16:43:28 -0800

    Update RunningOutOfEnergy.js

--- RunningOutOfEnergy.js
+++ RunningOutOfEnergy.js
@@ -3,6 +3,7 @@ function checkEnergy() {
    if(eval(UI.energyVal.value) >= 0) {
 		if (customSettings.playerDies.value == "true") {
    			alert ("Game Over\nYou ran out of energy");
+			location.reload(false);
 		}
 		else {
 			alert ("Player death is disabled");
