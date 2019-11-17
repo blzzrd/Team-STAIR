dd6711f2f0ab913ae9982ab6cd40ddcdf901a906
Isaiah Shikina <36141897+ishikina@users.noreply.github.com>
2019-11-17 05:59:30 -0800

    Updated Artifacts Configuration
    -Updated planet names
    -Added astroids & space stations
    -Removed input check(will use more efficent algorithm)
    -Able to set artifact object to celestial point (if testing, you can use "game_map.map[x][y].type.constructor.name" to determine object type)
    
    Missing
    -Collision check (IMPORTANT)
    -Sensors
    -Input check

--- celestial_artifacts.js
+++ celestial_artifacts.js
@@ -1,113 +1,100 @@
 class artifacts
 {
-	constructor(x, y)
+	constructor()
+	{}
+}
+
+class space_station extends artifacts
+{
+	constructor(supplies_cost)
 	{
-		this.x_coordinate = x;
-		this.y_coordinate = y;
+		super();
+		this.supplies_cost;
 	}
 }
 
 class asteroid extends artifacts
 {
-	constructor(x, y, game_over)
+	constructor(damage)
 	{
-		super(x,y);
-		this.game_over = game_over;
+		super();
+		this.damage;
 	}	
 }
 
 class pentium extends artifacts
 {
-	constructor(x, y, repair, recipe)
+	constructor(name, repair, recipe)
 	{
-		super(x, y);
+		super();
+		this.name = name;
 		this.repair = repair;
 		this.recipe = recipe;
 	}
 }
 
-function place_pentium(x, y, repair, recipe)
-{
-	if(map[x][y] != null)
-		return false;
-
-	map[x][y] = new pentium(x, y, repair, recipe);
-	return true;	
-}	
-
-function place_asteroid(x, y, game_over)
-{
-	if(map[x][y] != null)
-		return false;
-
-	map[x][y] = new asteroid(x, y, game_over);
-	return true;
-}
-
-function place()
+var input = [];
+/*
+function input_check()
 {
 	var pentium_1 = "" + document.getElementById("1_x_map").value + document.getElementById("1_y_map").value;
+	input[0] = pentium_1;	
 	var pentium_2 = "" + document.getElementById("2_x_map").value + document.getElementById("2_y_map").value;
+	input[1] = pentium_1;	
 	var pentium_3 = "" + document.getElementById("3_x_map").value + document.getElementById("3_y_map").value;
+	input[2] = pentium_1;	
 	var pentium_4 = "" + document.getElementById("4_x_map").value + document.getElementById("4_y_map").value;
+	input[3] = pentium_1;	
 	var pentium_5 = "" + document.getElementById("5_x_map").value + document.getElementById("5_y_map").value;
+	input[4] = pentium_1;	
 	var pentium_6 = "" + document.getElementById("6_x_map").value + document.getElementById("6_y_map").value;
+	input[5] = pentium_1;	
 	var pentium_7 = "" + document.getElementById("7_x_map").value + document.getElementById("7_y_map").value;
+	input[6] = pentium_1;	
+
+	if(document.getElementById("asteroid_1x").value
+	var asteroid_1 = "" + document.getElementById("asteroid_1x").value + document.getElementById("asteroid_1y").value;
+	var asteroid_2 = "" + document.getElementById("asteroid_2x").value + document.getElementById("asteroid_2y").value;
+	var asteroid_3 = "" + document.getElementById("asteroid_3x").value + document.getElementById("asteroid_3y").value;
+		
+
+}
+*/
 
 
-	if(pentium_1 == pentium_2 || pentium_1 == pentium_3 || pentium_1 == pentium_4 || pentium_1 == pentium_5 || pentium_1 == pentium_6 || pentium_1
-			== pentium_7)
-	{
-		alert("Two planets cannot be in the same space");	
-		return;
-	}
+function artifacts_configure(c_map)
+{
+	c_map.map[document.getElementById("1_x_map").value][document.getElementById("1_y_map").value].type = new pentium("Celeron", false, false);
+	c_map.map[document.getElementById("2_x_map").value][document.getElementById("2_y_map").value].type = new pentium("Xeon", false, false);
+	c_map.map[document.getElementById("3_x_map").value][document.getElementById("3_y_map").value].type = new pentium("Ryzen", false, false);
+	c_map.map[document.getElementById("4_x_map").value][document.getElementById("4_y_map").value].type = new pentium("Saturn", false, false);
+	c_map.map[document.getElementById("5_x_map").value][document.getElementById("5_y_map").value].type = new pentium("Mars", false, false);
+	c_map.map[document.getElementById("6_x_map").value][document.getElementById("6_y_map").value].type = new pentium("Jupiter", false, false);
+	c_map.map[document.getElementById("7_x_map").value][document.getElementById("7_y_map").value].type = new pentium("Pluto", false, false);
 
-
-	if(pentium_2 == pentium_3 || pentium_2 == pentium_4 || pentium_2 == pentium_5 || pentium_2 == pentium_6 || pentium_2 == pentium_7)
-	{
-		alert("Two planets cannot be in the same space");	
-		return;
-	}
-
-	if(pentium_3 == pentium_4 || pentium_3 == pentium_5 || pentium_3 == pentium_6 || pentium_3 == pentium_7)
-	{
-		alert("Two planets cannot be in the same space");	
-		return;
-	}
-
-	if(pentium_4 == pentium_5 || pentium_4 == pentium_6 || pentium_4 == pentium_7)
-	{
-		alert("Two planets cannot be in the same space");	
-		return;
-	}
-
-	if(pentium_5 == pentium_6 || pentium_5 == pentium_7)
-	{
-		alert("Two planets cannot be in the same space");	
-		return;
-	}
-
-	if(pentium_6 == pentium_7)
-	{
-		alert("Two planets cannot be in the same space");	
-		return;
-	}
-
-	place_pentium(documents.getElementById("1_x_map").value, documents.getElementById("1_y_map").value, false, false);
-	place_pentium(documents.getElementById("2_x_map").value, documents.getElementById("2_y_map").value, false, false);
-	place_pentium(documents.getElementById("3_x_map").value, documents.getElementById("3_y_map").value, false, false);
-	place_pentium(documents.getElementById("4_x_map").value, documents.getElementById("4_y_map").value, false, false);
-	place_pentium(documents.getElementById("5_x_map").value, documents.getElementById("5_y_map").value, false, false);
-	place_pentium(documents.getElementById("6_x_map").value, documents.getElementById("6_y_map").value, false, false);
-	place_pentium(documents.getElementById("7_x_map").value, documents.getElementById("7_y_map").value, false, false);
+	c_map.map[document.getElementById("asteroid_1x").value][document.getElementById("asteroid_1y").value].type = new asteroid(0);	
+	c_map.map[document.getElementById("asteroid_2x").value][document.getElementById("asteroid_2y").value].type = new asteroid(0);	
+	c_map.map[document.getElementById("asteroid_3x").value][document.getElementById("asteroid_3y").value].type = new asteroid(0);	
+	
+	c_map.map[document.getElementById("space_station_1x").value][document.getElementById("space_station_1y").value].type = new space_station(0);	
+	c_map.map[document.getElementById("space_station_2x").value][document.getElementById("space_station_2y").value].type = new space_station(0);	
+	c_map.map[document.getElementById("space_station_3x").value][document.getElementById("space_station_3y").value].type = new space_station(0);	
 }
 
-function hide_placement()
+function hide_asteroid()
 {
-	if(!document.getElementById('administrator').checked)
-		document.getElementById('place_pentium').style.display='none';
+	if(document.getElementById('asteroids').value == "False")
+		document.getElementById('place_asteroids').style.display='none';
 	else	
-		document.getElementById('place_pentium').style.display='block';
+		document.getElementById('place_asteroids').style.display='block';
+}
+
+function hide_space_station()
+{
+	if(document.getElementById('space_stations').value == "False")
+		document.getElementById('place_space_stations').style.display='none';
+	else	
+		document.getElementById('place_space_stations').style.display='block';
 }
 
 function collision()
@@ -120,15 +107,3 @@ function collision()
 	return;
 }
 
-/*	
-	function scanner(x, y, range)
-	{
-	for(var i = 0; i < range; ++i)
-	{
-		map
-
-	}  
-
-	return false;
-	}
- */
