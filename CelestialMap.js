fdf52f5415b9fcf81b8e3f0996e5da2ea339a2db
blzzrd <AlexCast001@gmail.com>
2019-11-19 13:06:48 -0800

    Added 'tracing' to movement, so any locations we skip over will be shown. Fixed map.

--- CelestialMap.js
+++ CelestialMap.js
@@ -12,9 +12,10 @@ class Celestial_Point {
         this.type = EMPTY;
         this.visited = false;
         this.visible = false;
+        this.curr = false;
     }
 
-    load(type=EMPTY, visited=false, visible=false) {
+    load(type = EMPTY, visited = false, visible = false) {
         this.type = type;
         this.visited = visited;
         this.visible = visible;
@@ -25,11 +26,17 @@ class Celestial_Point {
     }
 
     visit() {
-        this.visit = true;
+        this.visited = true;
+        this.show()
     }
 
     show() {
         this.visible = true;
+    }
+
+    current() {
+        this.visit();
+        this.curr = !this.curr;
     }
 
     hide() {
@@ -38,6 +45,10 @@ class Celestial_Point {
 
     display() {
         // is a pseudo-display for the celestial map function.
+        if (this.curr == true) {
+            return PLAYER;
+        }
+
         if (this.visible == true || this.visited == true) {
             return this.type;
         }
@@ -64,6 +75,8 @@ class Celestial_Map {
                 map[i][j] = new Celestial_Point();
             }
         }
+
+        map[gameSettings["xLocation"]][gameSettings["yLocation"]].current();
         return map;
     }
 
@@ -72,18 +85,20 @@ class Celestial_Map {
         // we have to iterate n^2 times EACH TIME the player 
         // makes a decision, so we definitely need to revise this.
         //alert(this.map);
-	document.getElementById("HtmlMap").rows=6;
-	document.getElementById("HtmlMap").cols=gameSettings["mapSize"]*2;
-	// below if is to allow for map to load on opening the page
-	if (gameSettings[supplies] == eval(UI.energyVal.value)) { 
-		this.map[gameSettings["mapSize"]-gameSettings["yLocation"]][gameSettings["xLocation"]].show();
-	}
-	// below else will reveal each node that you land on
-	else {
-		this.map[gameSettings["mapSize"]-eval(UI.yVal.value)][eval(UI.xVal.value)].show();
-	}
+        document.getElementById("HtmlMap").rows = 6;
+        document.getElementById("HtmlMap").cols = gameSettings["mapSize"] * 2;
+        // below if is to allow for map to load on opening the page
+        /*
+        if (gameSettings[supplies] == eval(UI.energyVal.value)) {
+            this.map[gameSettings["mapSize"] - gameSettings["yLocation"]][gameSettings["xLocation"]].show();
+        }
+        // below else will reveal each node that you land on
+        else {
+            this.map[gameSettings["mapSize"] - eval(UI.yVal.value)][eval(UI.xVal.value)].show();
+        }
+        */
         var print_string = '';
-        for (var i = 0; i < this.size; i++) {
+        for (var i = this.size - 1; i >= 0; i--) {
             print_string += '\n';
             for (var j = 0; j < this.size; j++) {
                 print_string += this.map[i][j].display() + ' ';
@@ -103,8 +118,124 @@ class Celestial_Map {
 
         // Otherwise we can add it as another parameter, default FALSE.
     }
+
+    traverse_map(angle, distance) {
+        // Angles: [0 = EAST, 90 = NORTH, 180 = WEST, 270 = SOUTH]
+        // Perform the computations based on the angles.
+
+        // Gather required variables.
+        var xCurrent = eval(document.getElementById("xVal").value);
+        var yCurrent = eval(document.getElementById("yVal").value);
+        var wormhole = false;
+        var max = eval(document.getElementById("mapSize").value);
+
+        this.map[xCurrent][yCurrent].current()
+
+        var newX, newY;
+
+        if (angle == 0) {
+            if (yCurrent + distance >= max) {
+                wormhole = true;
+                while (yCurrent != max - 1) {
+                    yCurrent += 1;
+                    this.map[xCurrent][yCurrent].visit();
+                }
+            }
+            else {
+                newX = xCurrent;
+                newY = yCurrent + distance;
+
+                while (distance >= 0) {
+                    this.map[xCurrent][yCurrent + distance].visit();
+                    distance -= 1;
+                }
+            }
+        }
+        else if (angle == 90) {
+            if (xCurrent + distance >= max) {
+                wormhole = true;
+                while (xCurrent != max - 1) {
+                    xCurrent += 1;
+                    this.map[xCurrent][yCurrent].visit();
+                }
+            }
+            else {
+                newX = xCurrent + distance;
+                newY = yCurrent;
+
+                while (distance >= 0) {
+                    this.map[xCurrent + distance][yCurrent].visit();
+                    distance -= 1;
+                }
+            }
+        }
+        else if (angle == 180) {
+            if (yCurrent < distance) {
+                wormhole = true;
+                while (yCurrent > 0) {
+                    yCurrent -= 1;
+                    this.map[xCurrent][yCurrent].visit();
+                }
+            }
+            else {
+                newX = xCurrent;
+                newY = yCurrent - distance;
+                
+                while (distance > 0) {
+                    this.map[xCurrent][yCurrent - distance].visit();
+                    distance -= 1;
+                }
+            }
+        }
+        else if (angle == 270) {
+            if (xCurrent < distance) {
+                wormhole = true;
+                while (xCurrent > 0) {
+                    xCurrent -= 1;
+                    this.map[xCurrent][yCurrent].visit();
+                }
+            }
+            else {
+                newX = xCurrent - distance;
+                newY = yCurrent;
+                
+                while (distance >= 0) {
+                    this.map[xCurrent - distance][yCurrent].show();
+                    distance -= 1;
+                }
+            }
+        }
+
+        // Prompt the message based on whether the wormhole exists or not.
+        if (wormhole == true) {
+            if (gameSettings["wormholeBehavior"] == "static") {
+                newX = gameSettings["xLocation"]
+                newY = gameSettings["yLocation"]
+            }
+            else {
+                newX = randCord(max);
+                newY = randCord(max);
+            }
+
+
+
+            alert("Spaceship traveled out of the galaxy. Warping through a wormhole.")
+        }
+        else {
+            alert("Moving Space Craft.");
+        }
+
+        // update the values as necessary.
+        document.getElementById('xVal').value = newX;
+        document.getElementById('yVal').value = newY;
+
+        this.map[newX][newY].current()
+        
+
+
+
+
+    }
 }
 
-
-var c_map = new Celestial_Map();
-
+var game_map = new Celestial_Map()
--- SpaceCraftMovement.js
+++ SpaceCraftMovement.js
@@ -1,91 +1,37 @@
-function moveSpacecraft(angle, distance) { 
+function moveSpacecraft(angle, distance) {
 
-   // Gather all the variables required.
-   distance = eval(distance.value)
-   xCurrent = eval(document.getElementById("xVal").value);
-   yCurrent = eval(document.getElementById("yVal").value);
-   wormhole = false;
-   max = eval(document.getElementById("mapSize").value);
+      // Gather all the variables required.
+      distance = eval(distance.value)
 
-   // Angles: [0 = EAST, 90 = NORTH, 180 = WEST, 270 = SOUTH]
-   // Perform the computations based on the angles.
-   if (angle == 0) {
-      if (xCurrent + distance > max) {
-	wormhole = true;
-      }
-      else {
-	newX = xCurrent + distance;
-	newY = yCurrent;
-      }
-   }
-   else if (angle == 90) {
-      if (yCurrent + distance > max) {
-	wormhole = true;
-      }
-      else {
-	newX = xCurrent;
-	newY = yCurrent + distance;
-      }
-   }
-   else if (angle == 180) {
-      if (xCurrent < distance) {
-	wormhole = true;
-      }
-      else {
-	newX = xCurrent - distance;
-	newY = yCurrent;
-      }
-   }
-   else if (angle == 270) {
-      if (yCurrent < distance) {
-	wormhole = true;
-      }
-      else {
-	newX = xCurrent;
-	newY = yCurrent - distance;
-      }
-   }
-
-   // Prompt the message based on whether the wormhole exists or not.
-   if(wormhole == true){
-	newX = randCord(max);
-	newY = randCord(max);
-	alert("Spaceship traveled out of the galaxy. Warping in a wormhole.")
-   }
-   else {
-        alert("Moving Space Craft.");
-   }
-
-   // update the values as necessary.
-   document.getElementById('xVal').value = newX;
-   document.getElementById('yVal').value = newY;
+      // Change MAP  
+      game_map.traverse_map(angle, distance)
 
 
-   decreaseEnergy(distance);
-   checkEnergy();
 
-   decreaseSupplies(distance);
-   checkSupplies();
+      decreaseEnergy(distance);
+      checkEnergy();
 
-   c_map.display_map();
+      decreaseSupplies(distance);
+      checkSupplies();
 
+      game_map.display_map();
 }
 
 function decreaseEnergy(distance) {
-   energy = eval(document.getElementById('energyVal').value);
+      energy = eval(document.getElementById('energyVal').value);
 
-   document.getElementById('energyVal').value = energy - (10 * distance);
+      document.getElementById('energyVal').value = energy - (10 * distance);
 }
 
-function decreaseSupplies() { 
-   supplies = eval(document.getElementById('suppliesVal').value);
+function decreaseSupplies() {
+      supplies = eval(document.getElementById('suppliesVal').value);
 
-   document.getElementById('suppliesVal').value = supplies - 2;
-} 
+      document.getElementById('suppliesVal').value = supplies - 2;
+}
 
 
 
-function randCord(max) { 
-	return Math.round(Math.random() * max);
+function randCord(max) {
+      return Math.round(Math.random() * max);
 }
 
