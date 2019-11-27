516ba296bf914dac700c15d747cabb15db81f2ba
SamYoungs <56419413+SamYoungs@users.noreply.github.com>
2019-11-26 18:05:00 -0800

    Creating load function for Docking

--- Space_Station_Docking.js
+++ Space_Station_Docking.js
@@ -5,13 +5,24 @@ class Docking {
         this.cost = 10;
     }
 
-    load_locations () {
-        if (document.getElementById('space_station') == true) {
-            this.locations.push[eval(place_space_stations.space_station_1x.value), eval(place_space_stations.space_station_1y.value)]
-            this.locations.push[eval(place_space_stations.space_station_2x.value), eval(place_space_stations.space_station_2y.value)]
-            this.locations.push[eval(place_space_stations.space_station_3x.value), eval(place_space_stations.space_station_3y.value)]
+    load_locations () { //loading the locations of space stations
+        for (var i = 0; i < 3; i++) {
+            this.locations[i]= [];
         }
-
+        this.locations[0][0] =document.getElementById("space_station_1x").value;
+        this.locations[0][1] =document.getElementById("space_station_1y").value;
+        this.locations[1][0] =document.getElementById("space_station_2x").value;
+        this.locations[1][1] =document.getElementById("space_station_2y").value;
+        this.locations[2][0] =document.getElementById("space_station_3x").value;
+        this.locations[2][1] =document.getElementById("space_station_3y").value;
+        //this.alert_location();
     }
-
+    alert_location(){ //Test function to see what is being loaded
+        alert("(" + this.locations[0][0] + "," + this.locations[0][1] + ") " +
+        "(" + this.locations[1][0] + "," + this.locations[1][1] + ") " +
+        "(" + this.locations[2][0] + "," + this.locations[2][1] + ")"
+        );
+    }
 }
+
+var Docking_Station = new Docking();
