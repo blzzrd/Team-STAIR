2201f1f791955569490d931ccb797f93d3bf1761
SamYoungs <56419413+SamYoungs@users.noreply.github.com>
2019-11-27 08:55:39 -0800

    updating space station docking

--- Space_Station_Docking.js
+++ Space_Station_Docking.js
@@ -17,6 +17,29 @@ class Docking {
         this.locations[2][1] =document.getElementById("space_station_3y").value;
         //this.alert_location();
     }
+
+    check_station () {
+        for (var i = 0; i < 3; i++) {
+            if (eval(UI.xVal.value) == this.locations[i][0] && eval(UI.yVal.value) == this.locations[i][1]) {
+                if(confirm("You have encountered a space station. \n Would you like to spend 10 credits for 20 energy?")) {
+                    this.purchase();
+                }
+            }
+        }
+    }
+
+    purchase() {
+        if (eval(UI.creditsVal.value) >= 10) {
+            var credits = eval(UI.creditsVal.value) - 10;
+            var energy = eval(UI.energyVal.value) + 20;
+            UI.energyVal.value = num.toString(energy);
+            UI.creditsVal.value = num.toString(credits);
+        }
+        else {
+            alert("You have too few credits");
+        }
+    }
+
     alert_location(){ //Test function to see what is being loaded
         alert("(" + this.locations[0][0] + "," + this.locations[0][1] + ") " +
         "(" + this.locations[1][0] + "," + this.locations[1][1] + ") " +
