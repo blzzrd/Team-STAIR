f9ddea4ab2929e02c30c4856b9291587da1586f9
SamYoungs <56419413+SamYoungs@users.noreply.github.com>
2019-12-03 13:27:43 -0800

    Update AsteroidCollision.js

--- AsteroidCollision.js
+++ AsteroidCollision.js
@@ -1,6 +1,6 @@
 class Asteroid_Collision {
     constructor() {
-        this.collision_damage = 20;
+        this.collision_damage = [];
         this.asteroid_location = [];
     }
 
@@ -14,20 +14,24 @@ class Asteroid_Collision {
         this.asteroid_location[1][1] =document.getElementById("asteroid_2y").value;
         this.asteroid_location[2][0] =document.getElementById("asteroid_3x").value;
         this.asteroid_location[2][1] =document.getElementById("asteroid_3y").value;
-        this.alert_asteroid();
+        this.collision_damage[0] = document.getElementById("damage_1").value;
+        this.collision_damage[1] = document.getElementById("damage_2").value;
+        this.collision_damage[2] = document.getElementById("damage_3").value;
     }
 
     check_asteroid () {
         for (var i = 0; i < 3; i++) {
             if (eval(UI.xVal.value) == this.asteroid_location[i][0] && eval(UI.yVal.value) == this.asteroid_location[i][1]) {
-                alert("You have crashed into an asteroid. \n You have taken " + this.collision_damage + " damage.");
-                this.crash_damage();
+                alert("You have crashed into an asteroid. \n You have taken " + this.collision_damage[i] + " damage.");
+                this.crash_damage(i);
+                this.asteroid_location[i][0] -= (this.asteroid_location[i][0]+1);
+                this.asteroid_location[i][1] -= (this.asteroid_location[i][1]+1);
             }
         }
     }
 
-    crash_damage () {
-        var new_supplies = eval(UI.suppliesVal.value) - this.collision_damage;
+    crash_damage (i) {
+        var new_supplies = eval(UI.suppliesVal.value) - this.collision_damage[i];
         document.getElementById('suppliesVal').value = new_supplies;
         checkSupplies();
     }
